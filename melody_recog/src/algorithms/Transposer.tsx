6143e0262557ca1257a6407f3a9ed428090e484c
Wingert Sarah <sw198@hdm-stuttgart.de>
2021-01-28 12:20:54 +0100

    Implementing minor to major transpose.

--- melody_recog/src/algorithms/Transposer.tsx
+++ melody_recog/src/algorithms/Transposer.tsx
@@ -3,6 +3,10 @@ import { SCALE } from "../models/calculationData";
 import { CIRCLE_OF_FIFTHS, CIRCLE_SIZE, FLATS, Key, MAJOR_KEY_SHIFT, MINOR_KEY_SHIFT, Mode, SHARPS } from "../models/keys";
 import { FrameNote, Note, Sign, SignedNote } from "../models/notes";
 
+const THIRD_HALF_STEPS = 3;
+const SIXTH_HALF_STEPS = 8;
+const SEPT_HALF_STEPS = 10;
+
 export function TransposeFundamentalNote(input: FrameNote[], prevBase: SignedNote, newBase: SignedNote, mode: Mode): { fifths: number, key: Key }{
     let halfSteps = SCALE.findIndex(val => val.equals(newBase)) - SCALE.findIndex(val => val.equals(prevBase));
     if (input.some(note => note.value.sign == Sign.FLAT)) {
@@ -37,13 +41,51 @@ export function TransposeFundamentalNote(input: FrameNote[], prevBase: SignedNot
     let index = CIRCLE_OF_FIFTHS.findIndex(val => val.equals(newBase));
     let key = mode == Mode.MINOR ? (index - MINOR_KEY_SHIFT) : ((index < CIRCLE_SIZE ? index - MAJOR_KEY_SHIFT : index - CIRCLE_OF_FIFTHS.length - MAJOR_KEY_SHIFT));
     if (key < 0) {
-        let signArray = key < 0 ? FLATS.slice(0, Math.abs(key)) : SHARPS.slice(0, key);
+        let signArray = FLATS.slice(0, Math.abs(key));
         ConvertSharpsToFlats(input, signArray);
     }
     return { fifths: key, key: new Key(newBase, mode) };
 }
-export function TransposeMode(input: FrameNote[]) {
-
+export function TransposeMode(input: FrameNote[], prevMode: Mode, currMode: Mode, baseNote: SignedNote): { fifths: number, key: Key } {
+    if (prevMode == currMode) {
+        let key = new Key(baseNote, currMode);
+        let index = CIRCLE_OF_FIFTHS.findIndex(val => val.equals(baseNote));
+        let fifths = currMode == Mode.MINOR ? (index - MINOR_KEY_SHIFT) : ((index < CIRCLE_SIZE ? index - MAJOR_KEY_SHIFT : index - CIRCLE_OF_FIFTHS.length - MAJOR_KEY_SHIFT));
+        return { fifths: fifths, key: key };
+    } else {
+        let index = SCALE.findIndex(val => val.equals(baseNote));
+        if (input.some(note => note.value.sign == Sign.FLAT)) {
+            ConvertFlatsToSharps(input);
+        }
+        let addOn = currMode == Mode.MAJOR? 0 : 1
+        let third = SCALE[(index + THIRD_HALF_STEPS + addOn) % SCALE.length];
+        let sixth = SCALE[(index + SIXTH_HALF_STEPS + addOn) % SCALE.length];
+        let sept = SCALE[(index + SEPT_HALF_STEPS + addOn) % SCALE.length];
+        input.filter(val => val.value.value != Note.BREAK)
+            .forEach(val => {
+                if (val.value.equals(third) || val.value.equals(sixth) || val.value.equals(sept)) {
+                    //Knowing that we are increasing the notes by a halfsteps,
+                    //this note and only this will change into a higher octave
+                    if (val.value.value == Note.B && currMode == Mode.MAJOR) {
+                        val.octave++;
+                    //Knowing that we are decreasing the notes by a halfsteps,
+                    //this note and only this will change into a lower octave
+                    } else if (val.value.value == Note.C && currMode == Mode.MINOR) {
+                        val.octave--;
+                    }
+                    let currIndex = SCALE.findIndex(note => note.equals(val.value));
+                    let change = currMode == Mode.MAJOR ? 1 : -1;
+                    val.value = SCALE[(currIndex + change) % SCALE.length];
+                }
+            });
+        let circleIndex = CIRCLE_OF_FIFTHS.findIndex(val => val.equals(baseNote));
+        let fifths = currMode == Mode.MINOR ? (circleIndex - MINOR_KEY_SHIFT) : ((circleIndex < CIRCLE_SIZE ? circleIndex - MAJOR_KEY_SHIFT : circleIndex - CIRCLE_OF_FIFTHS.length - MAJOR_KEY_SHIFT));
+        if (fifths < 0) {
+            let signArray = FLATS.slice(0, Math.abs(fifths))
+            ConvertSharpsToFlats(input, signArray);
+        }
+        return { fifths: fifths, key: new Key(baseNote, currMode) };
+    }
 }
 export function MoveOctave(input: FrameNote[], octaves: number) {
     input.forEach(val => val.octave += octaves);
